1297b1db69bf8e67404e07ee99783610049acffe
Daehico <empire_95@mail.ru>
2022-11-24 20:02:39 +0300

    Add explousian cars

--- Assets/Scripts/Car/CarHealth.cs
+++ Assets/Scripts/Car/CarHealth.cs
@@ -7,6 +7,20 @@ public class CarHealth : MonoBehaviour, IHitable
 {
     [SerializeField] private float _health;
     [SerializeField] private ParticleSystem _explouseianParticle;
+    [SerializeField] private Rigidbody _carRigidbody;
+    [SerializeField] private Rigidbody _firstLeftVehicle;
+    [SerializeField] private Rigidbody _secondLeftVehicle;
+    [SerializeField] private Rigidbody _firstRightVehicle;
+    [SerializeField] private Rigidbody _seocndRifgtVehicle;
+    [SerializeField] private MeshRenderer _carMeshRenderer;
+    [SerializeField] private Material _explouseionMaterial;
+    [SerializeField] private Vector3 _carExplouseVector;
+    [SerializeField] private Vector3 _rightVechicleExplouseForce;
+    [SerializeField] private Vector3 _leftVechicleExplouseForce;
+    [SerializeField] private float _cooldownDestroy;
+    [SerializeField] private ParticleSystem _fireParticle;
+    [SerializeField] private GameObject[] _disableObject;
+    [SerializeField] private float _enableFireDelay;
 
     public void ApplyDamage(float damage)
     {
@@ -25,6 +39,43 @@ public class CarHealth : MonoBehaviour, IHitable
 
     private void Explouse()
     {
+        _carRigidbody.isKinematic = false;
+        _firstLeftVehicle.isKinematic = false;
+        _firstRightVehicle.isKinematic = false;
+        _secondLeftVehicle.isKinematic = false;
+        _seocndRifgtVehicle.isKinematic = false;
+        _carRigidbody.AddForce(_carExplouseVector);
+        _firstLeftVehicle.AddForce(_leftVechicleExplouseForce);
+        _secondLeftVehicle.AddForce(_leftVechicleExplouseForce);
+        _firstRightVehicle.AddForce(_rightVechicleExplouseForce);
+        _secondLeftVehicle.AddForce(_rightVechicleExplouseForce);
+        _carMeshRenderer.material = _explouseionMaterial;
+
+        foreach(var disableObject in _disableObject)
+        {
+            disableObject.SetActive(false);
+        }
+
         _explouseianParticle.Play();
+        StartCoroutine(DestroyWithDelay());
+        StartCoroutine(EnableFireWithDelay());
+    }
+
+    private IEnumerator EnableFireWithDelay()
+    {
+        yield return new WaitForSeconds(_enableFireDelay);
+        _fireParticle.Play();
+    }
+
+    private IEnumerator DestroyWithDelay()
+    {
+        yield return new WaitForSeconds(_cooldownDestroy);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator TestDelay()
+    {
+        yield return new WaitForSeconds(5f);
+        Explouse();
     }
 }
