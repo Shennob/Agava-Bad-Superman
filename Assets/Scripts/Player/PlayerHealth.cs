a64d9e29bc0bd229118bf6c270414816139530d4
Daehico <empire_95@mail.ru>
2022-12-01 00:21:26 +0300

    Add dead

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,19 @@
 using BehaviorDesigner.Runtime.Tactical;
+using Opsive.UltimateCharacterController.Traits;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
+    private const string IsDie = "IsDie";
+
+    [SerializeField] private float _maxHealth;
     [SerializeField] private float _health;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private CharacterRespawner _respawner;
+    [SerializeField] private float _respawnTime;
+    [SerializeField] private WantedLevel _wantedLevel;
 
     private bool _isAlive = true;
 
@@ -15,6 +23,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
         if(_health <= 0)
         {
+            Die();
             _isAlive = false;
         }
     }
@@ -22,5 +31,20 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     public bool IsAlive()
     {
         return _isAlive;
+    }
+
+    private void Die()
+    {
+        _animator.SetBool(IsDie, true);
+        StartCoroutine(RespawnWithDelay());
+    }
+
+    private IEnumerator RespawnWithDelay()
+    {
+        yield return new WaitForSeconds(_respawnTime);
+        _respawner.Respawn();
+        _health = _maxHealth;
+        _wantedLevel.ResetWantedLevel();
+        _animator.SetBool(IsDie, false);
     }
 }
